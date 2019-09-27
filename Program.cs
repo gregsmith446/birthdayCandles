01b9c48df09ec01c47543657c9cba3cffd1db8c6
Greg <gregsmith446@gmail.com>
2019-09-26 20:18:05 -0400

    completed in 10 Minutes!

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace BirthdayCandles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Algorithm runCode = new Algorithm();
+
+            int[] input = new int[] { 4, 5, 5, 7, 1, 1, 0 };
+
+            var result = runCode.BirthdayCakeCandles(input);
+
+            Console.WriteLine(result);
+
+            // expected output is 1
+        }
+    }
+
+    class Algorithm
+    {
+        public int BirthdayCakeCandles(int[] ar)
+        {
+
+            int highestNumCounter = 0;
+            int highestNum = 0;
+
+            for (int i = 0; i < ar.Length; i++)
+            {
+                if (ar[i] > highestNum)
+                {
+                    highestNum = ar[i];
+                }
+            }
+
+            for (int i = 0; i < ar.Length; i++)
+            {
+                if (ar[i] == highestNum)
+                {
+                    highestNumCounter++;
+                }
+            }
+            return highestNumCounter;
+        }
+    }
+}
